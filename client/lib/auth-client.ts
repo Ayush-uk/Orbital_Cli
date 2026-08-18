b81edcd4bb68099700f3fd58b32a64908a7b509f
ayush-uk <ayushukenday@gmail.com>
2026-08-18 14:22:26 +0530

    configure production auth server

--- client/lib/auth-client.ts
+++ client/lib/auth-client.ts
@@ -2,9 +2,8 @@ import { deviceAuthorizationClient } from "better-auth/plugins"
 import { createAuthClient } from "better-auth/react"
 
 export const authClient = createAuthClient({
-    baseURL: "http://localhost:3005",
-    
-      plugins: [ 
-    deviceAuthorizationClient(), 
-  ], 
-})
+  baseURL: process.env.NEXT_PUBLIC_AUTH_URL || "http://localhost:3005",
+  plugins: [
+    deviceAuthorizationClient(),
+  ],
+});
