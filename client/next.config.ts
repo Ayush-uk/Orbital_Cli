4cc7b0abf3f291bab235b48e760ca459ce047371
ayushhhh <ayushukenday@gmail.com>
2026-08-18 20:21:02 +0530

    Update next.config.ts

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/auth/:path*",
+        destination: "https://orbital-cli-f83y.onrender.com/api/auth/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
